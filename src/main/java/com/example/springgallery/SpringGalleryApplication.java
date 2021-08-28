62fa29786e685c820b9720034b9812ec040b1535
nvh-gel <nvhien2703@outlook.com>
2021-08-28 11:34:57 +0700

    first commit

---
+++ src/main/java/com/example/springgallery/SpringGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGalleryApplication.class, args);
+    }
+
+}
